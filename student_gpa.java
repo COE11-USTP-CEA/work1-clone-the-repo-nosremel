fcdce5bdf337fa43137ab5a6e3dc2e5d1f14a633
Lemerson Cutab <lemersonjan@gmail.com>
2016-12-09 06:32:56 -0800

    Update student_gpa.java

--- student_gpa.java
+++ student_gpa.java
@@ -3,26 +3,23 @@ import java.util.List;
 class student_gpa{
 	public static void main(String[] args){
 		/*
-		student gabs = new student();
-		gabs.setName("Benjo Gaburno");
-		gabs.setDept("CEA");
-
+		student lem = new student();
+		lem.setName("Lemerson Jan Cutab");
+		lem.setDept("CEA");
 		subject oop_class = new subject();
 		oop_class.setSubjectName("Object Oriented Programming");
 		oop_class.setGrade(75.00f);
-		gabs.addSubject(oop_class);
-
+		lem.addSubject(oop_class);
 		subject trig_class = new subject();
 		trig_class.setSubjectName("Trigonometry");
 		trig_class.setGrade(61.17f);
-		gabs.addSubject(trig_class);
-
-		System.out.println("Student is " + gabs.getName());
-		System.out.println("Dept: " + gabs.getDept());
+		lem.addSubject(trig_class);
+		System.out.println("Student is " + lem.getName());
+		System.out.println("Dept: " + lem.getDept());
 		
 		System.out.println("Subjects of Student");
 		System.out.println("-------------------");
-		List<subject> subjects_ni_abs = gabs.getSubjects();
+		List<subject> subjects_ni_abs = lem.getSubjects();
 		
 		for(subject s : subjects_ni_abs){
 			System.out.println(" -- Subject: " + s.getSubjectName());
